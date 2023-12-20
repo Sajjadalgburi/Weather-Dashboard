2b95156b5c26ae700534c5ff20bacfc91d19b174
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-20 14:46:55 -0700

    Adding functunality to the search engine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
 // Allow the doc to load
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  // Grab the elements that are going to get manipulated
+  const searchBtn = document.querySelector(".searchBtn");
+  const cityName = document.querySelector(".search-input");
+
+  const getCityName = () => {
+    const cityNameValue = cityName.value.trim(); // grab and trim the vlaue of the input
+
+    if (!cityNameValue) return; // check if there is nothing, else return
+    console.log(cityNameValue);
+  };
+
+  searchBtn.addEventListener("click", getCityName); // on click display the getCityName function
+});
